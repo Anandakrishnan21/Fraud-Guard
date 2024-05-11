7ef5096d19bbe16c6536c7838ce9f12e2d0255ec
Anandakrishnan21 <anandakrishnandinej02@gmail.com>
2024-05-11 22:36:45 +0530

    [runcode]: route.js updated

--- src/app/api/runcode/route.js
+++ src/app/api/runcode/route.js
@@ -1,14 +1,10 @@
-import { exec } from "child_process";
+import { spawn } from "child_process";
 import path from "path";
 import fs from "fs";
 
 export async function POST(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method Not Allowed" });
-  }
-
   try {
-    const scriptFileName = "matrix.py";
+    const scriptFileName = "transpose.py";
     const documentsPath = path.join(process.env.USERPROFILE, "Documents");
     const scriptPath = path.join(documentsPath, scriptFileName);
 
@@ -17,14 +13,31 @@ export async function POST(req, res) {
       return res.status(404).json({ error: "Python script not found" });
     }
 
-    exec(`"${scriptPath}"`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing Python program: ${error}`);
-      } else {
-        console.log(`Python program output: ${stdout}`);
+    const pythonProcess = spawn("python", [scriptPath]);
+    let output = "";
+
+    pythonProcess.stdout.on("data", (data) => {
+      console.log(`Python program output: ${data}`);
+      output += data.toString();
+    });
+
+    pythonProcess.stderr.on("data", (data) => {
+      console.error(`Error executing Python program: ${data}`);
+      return res.status(500).json({ error: "Error executing Python program" });
+    });
+
+    pythonProcess.on("close", (code) => {
+      if (code !== 0) {
+        console.error(`Python script process exited with code ${code}`);
+        return res
+          .status(500)
+          .json({ error: "Python script process exited with error code" });
       }
+
+      res.status(200).json({ output });
     });
   } catch (error) {
     console.error("Error executing Python script:", error);
+    return res.status(500).json({ error: "Error executing Python script" });
   }
 }
